4801930d568f7e39001e2d9709803e7c34a7c0bf
Anujin Munkhbat <anujin.munhbat@gmail.com>
2018-05-08 15:11:41 -0700

    Update UserTest.java

--- src/test/java/codeu/model/data/UserTest.java
+++ src/test/java/codeu/model/data/UserTest.java
@@ -28,8 +28,8 @@ public class UserTest {
     Instant creation = Instant.now();
     String password = "password";
     String photo_url = "https://i1.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?resize=256%2C256&quality=100";
-	String user_skills = "test_skills";
-	String about = "test_about";
+    String user_skills = "test_skills";
+    String about = "test_about";
 	
     User user = new User(id, name, password, creation, photo_url, user_skills, about);
 
