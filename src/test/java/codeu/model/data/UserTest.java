24c7cd69aec34e8b40c40090cc316d157fe6f7d1
Sunbin Kim <Sunbini@Sunbins-MBP.attlocal.net>
2018-05-09 01:10:50 -0700

    one merge conflict fixed

--- src/test/java/codeu/model/data/UserTest.java
+++ src/test/java/codeu/model/data/UserTest.java
@@ -28,14 +28,9 @@ public class UserTest {
     Instant creation = Instant.now();
     String password = "password";
     String photo_url = "https://i1.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?resize=256%2C256&quality=100";
-<<<<<<< HEAD
 	String user_skills = "test_skills";
 	String about = "test_about";
     String email = "test_email";
-=======
-    String user_skills = "test_skills";
-    String about = "test_about";
->>>>>>> aeb23020cec16411e704d2a5850f550423223363
 	
     User user = new User(id, name, password, creation, photo_url, user_skills, about, email);
 
